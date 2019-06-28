0a27eb1f2c87bbe0d6660369e693104f5fd774f2
Shamila <shamila.m09@gmail.com>
2019-06-28 15:52:32 -0700

    setting up mongoDB n mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:56815/todos', { useNewUrlParser: true });
+//number from mongo window in terminal, todos is the db name
+const connection = mongoose.connection;
+
+connection.once('open', function(){
+	console.log("MongoDB database connection established successfully");
+} )
 
 app.listen(PORT, function(){
 	console.log('server is running on Port:' + PORT);
---
+++ backend/todo.model.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+
+
+//data comes in json form, need to set in String form (?) 
