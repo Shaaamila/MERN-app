993dfeafedb0d4bbf02dab6fc36d0ca065771a2d
Shamila <shamila.m09@gmail.com>
2019-06-27 22:09:43 -0700

    server created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-server.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, function(){
+	console.log('server is running on Port:' + PORT);
+});
+
+
+
