ace7d92e8327ab0e8c2278c88bad9cf5c2bdc085
Shamila <shamila.m09@gmail.com>
2019-07-27 17:14:16 -0700

    checking

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -24,12 +24,12 @@ export default class TodosList extends Component{
 
 	componentDidMount(){
 		axios.get('http://localhost:4000/heroku_0jxxb521/')
-			// .then(response => {
-			// 	this.setState({todos: response.data});
-			// })
-			// .catch(function (error){
-			// 	console.log(error);
-			// })
+			.then(response => {
+				this.setState({todos: response.data});
+			})
+			.catch(function (error){
+				console.log(error);
+			})
 	}
 	componentDidUpdate(){
 		axios.get('http://localhost:4000/heroku_0jxxb521/')
